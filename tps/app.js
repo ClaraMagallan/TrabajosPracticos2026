527726e69820b662627d4ba8d42a54421a55bf8d
alumno <alumno@Instituto.local>
2026-06-02 09:50:29 -0300

    skibidi
    hola

---
+++ tps/app.js
@@ -0,0 +1,11 @@
+//declarar variable
+let numero1
+
+//asignar valor
+numero1 =10
+
+//declarar y asignar al mismo tiempo
+let numero2 = 30
+
+//mostrar la informacion
+console.log(numero1 + numero1 + numero2)
