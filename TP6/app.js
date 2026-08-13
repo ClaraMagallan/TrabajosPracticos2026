5cde2db0e3b0c6f314be598d81cdece42248c6a5
mati1w <51399974@ivt.edu.ar>
2026-08-13 09:39:01 -0300

    UPDATE

--- TP6/app.js
+++ TP6/app.js
@@ -1,6 +1,24 @@
+// Cambio del texto
 let parrafo = document.querySelector('p')
 
-let btexto = document.querySelector ("cambiotexto")
+let btexto = document.querySelector ("#cambiotexto")
 btexto.onclick = function(){
-    btexto.textContent = "este texto cambio"
+    parrafo.textContent = "este texto cambio"
 }
+// Fondo del parrafo
+let bfondo = document.querySelector("#cambiofondo")
+bfondo.onclick = function() {
+parrafo.style.backgroundColor = "lightblue"
+
+}
+
+// Cambio de color
+let bcolor = document.querySelector("#cambiocolor")
+bcolor.onclick = function() {
+    parrafo.style.color = "Black"
+}
+ // Cambio letra
+ let bletra = document.querySelector ("#cambioletra")
+ bletra.onclick =function(){
+     parrafo.style.fontFamily = "IMPACT"
+ }
